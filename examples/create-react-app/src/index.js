9cce0b126bdfd18ffd4fa45e0662da64dca77957
Supi <supimon@gmail.com>
2017-12-02 09:14:49 +0530

    added textfields and guttergrid demos

--- examples/create-react-app/src/index.js
+++ examples/create-react-app/src/index.js
@@ -2,9 +2,17 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+// home
 import Index from './pages/index';
+// gutter grid demo
+import GutterGrid from './pages/grid-tester'
 
 const rootElement = document.querySelector('#root');
+/*// home
 if (rootElement) {
   render(<Index />, rootElement);
+}*/
+// gutter grid demo
+if (rootElement) {
+    render(<GutterGrid />, rootElement);
 }
